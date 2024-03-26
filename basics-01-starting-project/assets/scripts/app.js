c6049f7c9f93f20139c45d13675b9d429298697d
Patricia <patriciamrc96@gmail.com>
2024-03-26 17:57:25 -0300

    first commit

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defaultResult = 0
+
+let currentResult  = defaultResult;
+
+currentResult = (currentResult + 10) / 2;
+
+let calculationDescription = `(${defaultResult} + 10) / 2`;
+
+outputResult(currentResult, calculationDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,27 @@
+// Create two variables: One that holds a fictional user input (a number of
+//     your choice) and one that holds no value initially (e.g. "result").
+
+const fictionalNumber = 10;
+let result;
+
+//     Set the result variable to 18 plus the value stored in user input.
+
+result = fictionalNumber + 18;
+
+//     Add three additional lines of code where you change the result variable
+//     again (by subtracting a value of your choice, multiplying it and
+//     dividing it).
+
+result = result / 2;
+result = result * 3;
+result = result - 7;
+
+
+//     Think about the value stored in the user input variable you also created
+//     - did that value change?
+//      No
+
+//     <code>alert()</code> the result and the user input variables (in two
+//     separate <code>alert()</code> calls).
+alert(`My fictional number is ${fictionalNumber}.`);
+alert(`The result of my operations is ${result}.`);
